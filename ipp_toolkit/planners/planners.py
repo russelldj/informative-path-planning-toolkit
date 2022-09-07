c16cdbff00a987461a5b175e0ed11e4de9727606
David Russell <davidrussell327@gmail.com>
2022-09-07 12:39:08 -0400

    Add variance plus value planner

--- ipp_toolkit/planners/planners.py
+++ ipp_toolkit/planners/planners.py
@@ -1,7 +1,7 @@
 from ipp_toolkit.utils.sampling import get_flat_samples, get_flat_samples_start_stop
 import matplotlib.pyplot as plt
 from ipp_toolkit.world_models.world_models import BaseWorldModel
-from ipp_toolkit.config import PLANNING_RESOLUTION, VARIANCE_KEY
+from ipp_toolkit.config import MEAN_KEY, PLANNING_RESOLUTION, VARIANCE_KEY
 import numpy as np
 
 
@@ -31,6 +31,19 @@ class MostUncertainLocationPlanner(GridWorldPlanner):
         belief = world_model.sample_belief_array(self.planning_grid)
         var = belief[VARIANCE_KEY]
         most_uncertain_indices = np.argsort(var)[-n_steps:]
+        most_uncertain_locs = self.planning_grid[most_uncertain_indices]
+        most_uncertain_locs = np.flip(most_uncertain_locs, axis=0)
+        return most_uncertain_locs
+
+
+class HighestUpperBoundLocationPlanner(GridWorldPlanner):
+    def plan(self, world_model: BaseWorldModel, n_steps=1, variance_scale=100):
+        belief = world_model.sample_belief_array(self.planning_grid)
+        var = belief[VARIANCE_KEY]
+        mean = belief[MEAN_KEY]
+        lower_bound = mean + variance_scale * var
+
+        most_uncertain_indices = np.argsort(lower_bound)[-n_steps:]
         most_uncertain_locs = self.planning_grid[most_uncertain_indices]
         most_uncertain_locs = np.flip(most_uncertain_locs, axis=0)
         return most_uncertain_locs
