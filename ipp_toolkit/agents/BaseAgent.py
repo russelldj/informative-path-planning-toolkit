27ce802b0084869c234e717bb5b42945a71e133d
David Russell <davidrussell327@gmail.com>
2022-11-08 11:11:56 -0500

    Remove boilerplate in stable baselines agent creation

--- ipp_toolkit/agents/BaseAgent.py
+++ ipp_toolkit/agents/BaseAgent.py
@@ -1,15 +1,57 @@
+import os
+from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.callbacks import CheckpointCallback
+
+
 class BaseAgent:
     def __init__(self, action_space):
-        pass
+        self.model = None
+        self.rl_alg_class = None
 
     def get_name(self):
-        raise NotImplementedError()
+        return self.name
 
     def train(self, env, cfg):
-        raise NotImplementedError()
+        model_dir = cfg["model_dir"]
+        log_dir = cfg["log_dir"]
+        num_par = cfg["num_par"]
+        save_freq = cfg["save_freq"]
+        total_timesteps = cfg["total_timesteps"]
+
+        if not os.path.exists(model_dir):
+            os.mkdir(model_dir)
+
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+
+        dummy_env = DummyVecEnv([lambda: env] * num_par)
+        self._create_model(cfg, dummy_env)
+
+        checkpoint_callback = CheckpointCallback(
+            save_freq=save_freq,
+            save_path=log_dir,
+            name_prefix=self.model_name + "_checkpoint",
+            save_replay_buffer=True,
+            save_vecnormalize=True,
+        )
+
+        self.model.learn(
+            total_timesteps=int(total_timesteps),
+            progress_bar=True,
+            callback=checkpoint_callback,
+        )
 
     def load_model(self, model_dir):
-        raise NotImplementedError()
+        model_path = os.path.join(model_dir, self.model_name)
+        self.model = self.rl_alg_class.load(model_path)
 
     def get_action(self, observation):
+
+        if self.model is None:
+            raise RuntimeError("Need to load model before getting action")
+
+        return self.model.predict(observation, deterministic=True)
+
+    def _create_model(self, cfg, env):
+        """This needs to be defined in the subclass"""
         raise NotImplementedError()
--- ipp_toolkit/agents/PPOAgent.py
+++ ipp_toolkit/agents/PPOAgent.py
@@ -1,9 +1,4 @@
-import os
-
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv
-from stable_baselines3.common.callbacks import CheckpointCallback
-from stable_baselines3.common.env_checker import check_env
 
 
 from ipp_toolkit.agents.BaseAgent import BaseAgent
@@ -15,60 +10,19 @@ class PPOAgent(BaseAgent):
         self.policy = "CnnPolicy"
         self.model_name = "ppo_model"
         self.action_space = action_space
+        self.rl_alg_class = PPO
         self.model = None
 
-    def get_name(self):
-        return self.name
-
-    def train(self, env, cfg):
-        num_par = cfg["num_par"]
+    def _create_model(self, cfg, env):
         learning_rate = cfg["learning_rate"]
         n_steps = cfg["n_steps"]
-        total_timesteps = cfg["total_timesteps"]
         verbose = cfg["verbose"]
-        save_freq = cfg["save_freq"]
-        model_dir = cfg["model_dir"]
-        log_dir = cfg["log_dir"]
 
-        if not os.path.exists(model_dir):
-            os.mkdir(model_dir)
-
-        if not os.path.exists(log_dir):
-            os.mkdir(log_dir)
-
-        dummy_env = DummyVecEnv([lambda: env] * num_par)
-
-        model = PPO(
+        self.model = self.rl_alg_class(
             self.policy,
-            dummy_env,
+            env,
             learning_rate=learning_rate,
             n_steps=n_steps,
             verbose=verbose,
         )
 
-        checkpoint_callback = CheckpointCallback(
-            save_freq=save_freq,
-            save_path=log_dir,
-            name_prefix=self.model_name + "_checkpoint",
-            save_replay_buffer=True,
-            save_vecnormalize=True,
-        )
-
-        model.learn(
-            total_timesteps=int(total_timesteps),
-            progress_bar=True,
-            callback=checkpoint_callback,
-        )
-
-        model_path = os.path.join(model_dir, self.model_name)
-        model.save(model_path)
-
-    def load_model(self, model_dir):
-        model_path = os.path.join(model_dir, self.model_name)
-        self.model = PPO.load(model_path)
-
-    def get_action(self, observation):
-        if self.model is None:
-            raise RuntimeError("Need to load model before getting action")
-
-        return self.model.predict(observation, deterministic=True)
