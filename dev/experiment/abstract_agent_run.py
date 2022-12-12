54b4c5f2f7c133889aec8869e866c6eed8f87b08
hfreecmu <hfreeman@andrew.cmu.edu>
2022-12-12 01:31:21 -0500

    dqn_trial

--- dev/experiment/abstract_agent_run.py
+++ dev/experiment/abstract_agent_run.py
@@ -8,7 +8,7 @@ ex = Experiment("rl_train_test")
 
 @ex.config
 def config():
-    agent_types = ["MB", "random"]  # Which agents to train or test on
+    agent_types = ["DQN", "random"]  # Which agents to train or test on
     policy = "MlpPolicy"  # What policy to use, can also be CNN
     num_trials = 20  # How many test runs to run
     vis_dir = "vis"  # Where to save visualization
@@ -27,7 +27,7 @@ def config():
     rew_diff_num_visited_scale = 0.0  # ?
     write_video = False  # Save out results video
     map_seed = None  # Random seed for the map
-    action_space_discretization = None  # Or an int specifying how many samples per axis
+    action_space_discretization = 7  # Or an int specifying how many samples per axis
     observation_space_discretization = 7 
     map_lower_offset = 0.5  # The lowest value in the map
     use_interpolation_model = (
