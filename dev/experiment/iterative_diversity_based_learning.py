e2635cb943c2cc270f6e1af3eac3c4158fda3a71
David Russell <davidrussell327@gmail.com>
2022-12-30 08:41:53 -0500

    Working experiments

--- dev/experiment/iterative_diversity_based_learning.py
+++ dev/experiment/iterative_diversity_based_learning.py
@@ -99,11 +99,12 @@ def compute_greenness(data_manager, vis=False):
     img = data_manager.image
     magnitude = np.linalg.norm(img[..., 0::2], axis=2)
     green = img[..., 1]
-    greenness = green.astype(float) / (magnitude.astype(float) + 1)
+    greenness = green.astype(float) / (magnitude.astype(float) + 0.00001)
     greenness = np.clip(greenness, 0, 4) / 4
+    greenness[np.logical_not(data_manager.mask)] = np.nan
     if vis:
         fig, axs = plt.subplots(1, 2)
-        plt.colorbar(axs[0].imshow(img))
+        axs[0].imshow(img)
         plt.colorbar(axs[1].imshow(greenness), ax=axs[1])
         axs[0].set_title("Original image")
         axs[1].set_title("Psuedo-label")
@@ -122,7 +123,7 @@ def run_forest_ortho(data_folder, n_clusters=12, visit_n_locations=8, vis=False)
     ]
 
     data_manager = MaskedLabeledImage(ortho, mask_filename)
-    data_manager.label = compute_greenness(data_manager)
+    data_manager.label = compute_greenness(data_manager, vis=True)
     run_exp(
         data_manager,
         n_clusters=n_clusters,
@@ -134,11 +135,11 @@ def run_forest_ortho(data_folder, n_clusters=12, visit_n_locations=8, vis=False)
 def run_forest_gmap(n_clusters=12, visit_n_locations=8, vis=False):
     ortho = "/home/frc-ag-1/dev/research/informative-path-planning-toolkit/data/maps/safeforest_gmaps/google_earth_site.png"
 
-    data_manager = MaskedLabeledImage(ortho)
+    data_manager = MaskedLabeledImage(ortho, blur_sigma=4)
     # plt.imshow(label)
     # plt.colorbar()
     # plt.show()
-    data_manager.label = compute_greenness(data_manager)
+    data_manager.label = compute_greenness(data_manager, vis=True)
     run_exp(
         data_manager,
         n_clusters=n_clusters,
@@ -149,11 +150,11 @@ def run_forest_gmap(n_clusters=12, visit_n_locations=8, vis=False):
 
 if __name__ == "__main__":
     args = parse_args()
-    run_forest_ortho(
-        forest_folder,
-        n_clusters=args.n_clusters,
-        visit_n_locations=args.visit_n_locations,
-    )
+    # run_forest_ortho(
+    #    forest_folder,
+    #    n_clusters=args.n_clusters,
+    #    visit_n_locations=args.visit_n_locations,
+    # )
     run_forest_gmap(
         n_clusters=args.n_clusters, visit_n_locations=args.visit_n_locations,
     )
