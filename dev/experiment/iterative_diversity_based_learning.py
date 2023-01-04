2c0fb35db57894b2117b9637368dc4e61457c49e
David Russell <davidrussell327@gmail.com>
2023-01-04 17:59:59 -0500

    Move common resources for comparisions into experiments

--- dev/experiment/iterative_diversity_based_learning.py
+++ dev/experiment/iterative_diversity_based_learning.py
@@ -2,32 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-from ipp_toolkit.config import DATA_FOLDER, ERROR_IMAGE, VIS
+from ipp_toolkit.config import DATA_FOLDER, VIS
 from argparse import ArgumentParser
 from ipp_toolkit.data.MaskedLabeledImage import (
     ImageNPMaskedLabeledImage,
     torchgeoMaskedDataManger,
 )
-from ipp_toolkit.planners.diversity_planner import (
-    DiversityPlanner,
-    BatchDiversityPlanner,
-)
-from ipp_toolkit.predictors.masked_image_predictor import (
-    MaskedLabeledImagePredictor,
-    EnsembledMaskedLabeledImagePredictor,
-)
-from ipp_toolkit.planners.masked_planner import GridMaskedPlanner, RandomMaskedPlanner
-from ipp_toolkit.config import (
-    TOP_FRAC_MEAN_ERROR,
-    UNCERTAINTY_KEY,
-    MEAN_KEY,
-    MEAN_ERROR_KEY,
-)
-from imageio import imread, imwrite
-from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPRegressor, MLPClassifier
-from sklearn.linear_model import LinearRegression
-from skimage.color import rgb2hsv
+from ipp_toolkit.experiments.comparing_ipp_approaches import run_repeated_exp
 
 
 def parse_args():
@@ -43,112 +24,6 @@ forest_folder = Path(DATA_FOLDER, "maps/safeforest")
 safeforest_gmaps_folder = Path(DATA_FOLDER, "maps/safeforest_gmaps")
 aiira_folder = Path(DATA_FOLDER, "maps/aiira")
 yellowcat_folder = Path(DATA_FOLDER, "maps/yellowcat")
-
-
-def plot_errors(all_l2_errors, run_tag):
-    all_l2_errors = np.vstack(all_l2_errors)
-    np.save(f"vis/iterative_exp/{run_tag}_errors.npy", all_l2_errors)
-    mean = np.mean(all_l2_errors, axis=0)
-    std = np.std(all_l2_errors, axis=0)
-    x = np.arange(len(mean))
-    plt.plot(x, mean, label=f"{run_tag} mean")
-    plt.fill_between(
-        x, mean - std, mean + std, label=f"{run_tag} one std bound", alpha=0.3
-    )
-
-
-def run_repeated_exp(n_trials=10, **kwargs):
-    diversity_planner_result = [
-        run_exp(use_random_planner=False, **kwargs) for _ in range(n_trials)
-    ]
-    random_planner_result = [
-        run_exp(use_random_planner=True, **kwargs) for _ in range(n_trials)
-    ]
-
-    plt.close()
-    plt.cla()
-    plt.clf()
-    plot_errors(random_planner_result, "Random planner")
-    plot_errors(diversity_planner_result, "Diversity planner")
-    plt.legend()
-    plt.xlabel("Number of sampling iterations")
-    plt.ylabel("Test error")
-    plt.savefig("vis/iterative_exp/final_values.png")
-    plt.show()
-
-
-def run_exp(
-    data_manager,
-    n_clusters=12,
-    visit_n_locations=8,
-    vis=VIS,
-    use_random_planner=False,
-    regression_task=False,
-    n_flights=5,
-    vmin=0,
-    vmax=9,
-    cmap="tab10",
-    error_key=MEAN_ERROR_KEY,
-):
-    # Chose which model to use
-    if regression_task:
-        model = MLPRegressor()
-    else:
-        model = MLPClassifier()
-
-    # Chose which planner to use
-    if use_random_planner:
-        planner = RandomMaskedPlanner(data_manager)
-    else:
-        planner = BatchDiversityPlanner(data_manager, n_candidate_locations=n_clusters)
-    # Create the predictor
-    predictor = EnsembledMaskedLabeledImagePredictor(
-        data_manager, model, classification_task=True, n_ensemble_models=7
-    )
-
-    errors = []
-    # No prior interestingess
-    interestingness_image = None
-
-    for i in range(n_flights):
-        savepath = f"vis/iterative_exp/no_revisit_plan_iter_{i}.png"
-        plan = planner.plan(
-            interestingness_image=interestingness_image,
-            visit_n_locations=visit_n_locations,
-            vis=VIS,
-            savepath=savepath,
-        )
-        print(f"Saving to {savepath}")
-        # Remove duplicate entry
-        plan = plan[:-1]
-        values = data_manager.sample_batch(plan, assert_valid=True, vis=VIS)
-        predictor.update_model(plan, values)
-
-        pred = predictor.predict_values_and_uncertainty()
-        interestingness_image = pred[UNCERTAINTY_KEY]
-        pred_values = pred[MEAN_KEY]
-        error_dict = predictor.get_errors()
-        errors.append(error_dict[error_key])
-        # Visualization
-        if vis:
-            fig, axs = plt.subplots(2, 2)
-            axs[0, 0].imshow(data_manager.image)
-            plt.colorbar(
-                axs[0, 1].imshow(data_manager.label, vmin=vmin, vmax=vmax, cmap=cmap),
-                ax=axs[0, 1],
-            )
-            plt.colorbar(
-                axs[1, 0].imshow(pred_values, vmin=vmin, vmax=vmax, cmap=cmap),
-                ax=axs[1, 0],
-            )
-            plt.colorbar(axs[1, 1].imshow(error_dict[ERROR_IMAGE]), ax=axs[1, 1])
-            axs[0, 0].set_title("Image")
-            axs[0, 1].set_title("Label")
-            axs[1, 0].set_title("Pred label")
-            axs[1, 1].set_title("Pred error")
-            plt.savefig(f"vis/iterative_exp/pred_iter_{i}.png")
-            plt.close()
-    return errors
 
 
 def compute_greenness(data_manager, vis=VIS):
