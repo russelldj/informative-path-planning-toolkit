41b530ee0c4f9cf5dc4e3309f1e17887cf0d7d4c
David Russell <davidrussell327@gmail.com>
2023-01-13 16:47:52 -0500

    add visualization helpers
    For saving and tick mark management

---
+++ ipp_toolkit/visualization/utils.py
@@ -0,0 +1,39 @@
+from ubelt import ensuredir
+from pathlib import Path
+import matplotlib.pyplot as plt
+from ipp_toolkit.config import FIG_SIZE, PAUSE_DURATION
+
+
+def show_or_save_plt(savepath=None, pause_duration=None, fig_size=FIG_SIZE):
+    """
+    Can either save to a file, pause, or showG
+    """
+    if savepath is not None:
+        plt.gcf().set_size_inches(*fig_size)
+        savepath = Path(savepath)
+        ensuredir(savepath.parent)
+        plt.savefig(savepath)
+        plt.close()
+        return
+
+    if pause_duration is not None:
+        plt.pause(pause_duration)
+        plt.close()
+        return
+
+    plt.show()
+
+
+def remove_ticks():
+    plt.tick_params(
+        axis="both",
+        which="both",
+        bottom=False,
+        top=False,
+        left=False,
+        right=False,
+        labelbottom=False,
+        labeltop=False,
+        labelleft=False,
+        labelright=False,
+    )
