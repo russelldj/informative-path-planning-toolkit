8e84ad7379ae3d33fcad284c50547662dc0605f8
David Russell <davidrussell327@gmail.com>
2023-02-06 13:36:29 -0500

    Add documentation and downsample classes to most prevalent 10

--- ipp_toolkit/data/domain_data.py
+++ ipp_toolkit/data/domain_data.py
@@ -29,6 +29,22 @@ def compute_greenness(data_manager, vis=VIS):
     return greenness
 
 
+def take_top_k_classes(label_map: np.ndarray, k: int):
+    """
+    The label map is a class label per pixel. This returns a new map where each of the top k-1 classes
+    are preserved. All other classes are combined into one "background" class.
+    """
+    unique, counts = np.unique(label_map, return_counts=True)
+    sorted_inds = np.argsort(counts)
+    sorted_unique = np.flip(unique[sorted_inds])
+
+    # set up everything with the background class
+    output_label_map = np.ones_like(label_map) * (k - 1)
+    for i, class_index in enumerate(sorted_unique[: (k - 1)]):
+        output_label_map[label_map == class_index] = i
+    return output_label_map
+
+
 class CoralLandsatClassificationData(ImageNPMaskedLabeledImage):
     def __init__(
         self,
@@ -160,7 +176,16 @@ class AIIRAGreennessRegresssionData(ImageNPMaskedLabeledImage):
 
 class CupriteASTERMineralClassificationData(ImageNPMaskedLabeledImage):
     """
-    Obtained from Alberto Candela
+    Data from the Cuprite, NV mining area. This location is well-studied in geology so there is 
+    extensive remote sensing and field work done about the area. This data was most extensively
+    used by Alberto Candela, now at NASA JPL. 
+
+    This data has features from the ASTER satellite measurements. I believe these observations
+    are upsampled from 15m/px to 3.5m/px to match that of the AVIRIS data product.
+
+    The label maps are obtained from the Tetracorder software to the best of my knowledge. The 
+    label maps are reduced from 215 classes to the top 10, preserving those 9 most prevalent classes
+    and the last class is an aggregation of everything else. 
     """
 
     def __init__(
@@ -172,12 +197,13 @@ class CupriteASTERMineralClassificationData(ImageNPMaskedLabeledImage):
         super().__init__(
             image=image,
             label=label,
-            vis_vmin=0,
-            vis_vmax=215,
-            cmap="tab20",
-            n_classes=216,
+            vis_vmin=-0.5,
+            vis_vmax=9.5,
+            cmap="tab10",
+            n_classes=10,
         )
         # Condense the channels
+        self.label = take_top_k_classes(self.label, 10)
 
     def download(self):
         pull_dvc_data(Path(DATA_FOLDER, "maps/cuprite"))
@@ -185,7 +211,10 @@ class CupriteASTERMineralClassificationData(ImageNPMaskedLabeledImage):
 
 class CupriteAVIRISASTERMineralClassificationData(ImageNPMaskedLabeledImage):
     """
-    Obtained from Alberto Candela
+    See CupriteASTERMineralClassificationData for a general description.
+
+    This data was obtained from the AVIRIS aerial collection and spectrally downsampled
+    to match that of ASTER
     """
 
     def __init__(
@@ -199,17 +228,48 @@ class CupriteAVIRISASTERMineralClassificationData(ImageNPMaskedLabeledImage):
             label=label,
             use_zero_allchannels_mask=True,
             drop_last_image_channel=False,
-            vis_vmin=0,
-            vis_vmax=215,
-            cmap="tab20",
-            n_classes=216,
+            vis_vmin=-0.5,
+            vis_vmax=9.5,
+            cmap="tab10",
+            n_classes=10,
         )
+        self.label = take_top_k_classes(self.label, 10)
+
+    def download(self):
+        pull_dvc_data(Path(DATA_FOLDER, "maps/cuprite"))
+
+
+class CupriteAVIRISMineralClassificationData(ImageNPMaskedLabeledImage):
+    """
+    See CupriteASTERMineralClassificationData for a general description.
+
+    This data was obtained from the AVIRIS aerial collection.
+    """
+
+    def __init__(
+        self,
+        image=Path(DATA_FOLDER, "maps/cuprite/aviris/aviris_cube_2um_norm.npy"),
+        label=Path(DATA_FOLDER, "maps/cuprite/labels/mineral.npy"),
+    ):
+        # TODO update plotting options
+        super().__init__(
+            image=image,
+            label=label,
+            use_zero_allchannels_mask=True,
+            drop_last_image_channel=False,
+            vis_vmin=-0.5,
+            vis_vmax=9.5,
+            cmap="tab10",
+            n_classes=10,
+        )
+        self.label = take_top_k_classes(self.label, 10)
 
     def download(self):
         pull_dvc_data(Path(DATA_FOLDER, "maps/cuprite"))
 
 
 ALL_LABELED_DOMAIN_DATASETS = {
+    "cuprite_aviris": CupriteAVIRISMineralClassificationData,
     "cuprite_aster": CupriteASTERMineralClassificationData,
     "cuprite_aster_aviris": CupriteAVIRISASTERMineralClassificationData,
     "aiira": AIIRAGreennessRegresssionData,
