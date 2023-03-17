f029bc1929f9f0a2e407dafe26846fa1ed413748
David Russell <davidrussell327@gmail.com>
2023-03-17 13:38:12 -0400

    Streamline prediction process

--- ipp_toolkit/predictors/convolutional_predictors.py
+++ ipp_toolkit/predictors/convolutional_predictors.py
@@ -1,16 +1,11 @@
-from ipp_toolkit.predictors.masked_image_predictor import MaskedLabeledImagePredictor
-
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn.functional as F
-import numpy as np
-from ipp_toolkit.config import TORCH_DEVICE
-import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from ipp_toolkit.visualization.utils import add_colorbar
-from sklearn.manifold import TSNE
-from sklearn.svm import LinearSVC
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+
+from ipp_toolkit.config import TORCH_DEVICE
+from ipp_toolkit.predictors.masked_image_predictor import MaskedLabeledImagePredictor
 
 
 class MOSAIKImagePredictor(MaskedLabeledImagePredictor):
@@ -29,24 +24,32 @@ class MOSAIKImagePredictor(MaskedLabeledImagePredictor):
         """
         Args:
             kernel_width: must be odd
-            n_features: note that some will be the negation 
+            n_features: note that some will be the negation
+            masked_labeled_image,
+            prediction_model: Unused
+            use_locs_for_prediction: Unused
+            classification_task: Unused
+            n_features: The number of features corresponding to the random kernels
+            kernel_width: width in pixels of the random kernel
+            bias: bias during convolution,
+            spatial_pooling_factor: How much to downsample the feature map
+            device: Which device to use for computation
         """
-        self.device = device
-
-        self.masked_labeled_image = masked_labeled_image
         assert kernel_width % 2 == 1
+
+        self.device = device
+        self.masked_labeled_image = masked_labeled_image
         self.kernel_width = kernel_width
         self.n_features = n_features
         self.spatial_pooling_factor = spatial_pooling_factor
 
-        self.biases = torch.zeros(n_features // 2, requires_grad=False) + bias
-        self.biases = self.biases.to(self.device)
-
-        self._compute_features()
+        self.biases = (torch.zeros(n_features // 2, requires_grad=False) + bias).to(
+            self.device
+        )
+        self.compressed_spatial_features = None
 
     def _compute_features(self, vis=True):
-        """
-        """
+        """ """
         mean = np.mean(self.masked_labeled_image.image, axis=(0, 1))
         std = np.std(self.masked_labeled_image.image, axis=(0, 1))
         mean, std = [np.expand_dims(x, (0, 1)) for x in (mean, std)]
@@ -54,7 +57,7 @@ class MOSAIKImagePredictor(MaskedLabeledImagePredictor):
 
         self._compute_kernels()
         self.features = self._forward()
-        self._compress_features()
+        return self._compress_features()
 
     def _compute_kernels(self):
         i_size, j_size = self.normalized_image.shape[:2]
@@ -118,48 +121,17 @@ class MOSAIKImagePredictor(MaskedLabeledImagePredictor):
         output = torch.permute(output, (1, 2, 0))
         return output.detach().cpu().numpy()
 
-    def _compress_features(self, vis=True, num_tsne=10000):
+    def _compress_features(self):
         flat_features = np.reshape(self.features, (-1, self.features.shape[-1]))
         pca = PCA(n_components=6)
         compressed_features = pca.fit_transform(flat_features)
         compressed_spatial_features = np.reshape(
             compressed_features, self.features.shape[:2] + (-1,)
         )
-        if vis:
-            flat_labels = self.masked_labeled_image.label.flatten()
+        return compressed_spatial_features
 
-            X_train, X_test, y_train, y_test = train_test_split(
-                compressed_features, flat_labels, test_size=0.9999, random_state=42
-            )
-            svc = LinearSVC()
-            svc.fit(X_train, y_train)
-            y_pred = svc.predict(X_test)
+    def predict_values(self):
+        if self.compressed_spatial_features is None:
+            self.compressed_spatial_features = self._compute_features()
 
-            conf = confusion_matrix(y_test, y_pred)
-            add_colorbar(plt.imshow(conf))
-            plt.show()
-
-            print("starting tsne visualization")
-            tsne = TSNE()
-            sampled_points = np.random.choice(compressed_features.shape[0], num_tsne)
-
-            tsne_embedded_compressed_features = tsne.fit_transform(
-                compressed_features[sampled_points, :]
-            )
-            plt.scatter(
-                tsne_embedded_compressed_features[:, 0],
-                tsne_embedded_compressed_features[:, 1],
-                c=flat_labels[sampled_points],
-                vmin=-0.5,
-                vmax=9.5,
-                cmap="tab10",
-            )
-            plt.show()
-
-            for i in range(0, compressed_spatial_features.shape[-1]):
-                f, ax = plt.subplots(1, 2)
-                chip = compressed_spatial_features[..., i : i + 1]
-                print(np.mean(chip, (0, 1)))
-                ax[0].imshow(self.masked_labeled_image.image[..., :3])
-                add_colorbar(ax[1].imshow(chip))
-                plt.show()
+        return self.compressed_spatial_features
