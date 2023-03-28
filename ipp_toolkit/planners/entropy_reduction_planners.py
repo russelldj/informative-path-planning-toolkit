45c191eccdf2024515ce17e6b4293902069e2940
David Russell <davidrussell327@gmail.com>
2023-03-28 17:42:29 -0400

    Working version which looks for highest entropy

--- ipp_toolkit/planners/entropy_reduction_planners.py
+++ ipp_toolkit/planners/entropy_reduction_planners.py
@@ -6,6 +6,7 @@ from ipp_toolkit.predictors.masked_image_predictor import (
 from ipp_toolkit.predictors.masked_image_predictor import (
     UncertainMaskedLabeledImagePredictor,
 )
+from ipp_toolkit.visualization.utils import add_colorbar
 from scipy.spatial.distance import cdist, pdist
 from ipp_toolkit.planners.utils import order_locations_tsp
 from ipp_toolkit.config import UNCERTAINTY_KEY
@@ -17,6 +18,8 @@ from copy import deepcopy
 def image_argmax(img: np.ndarray, n_samples: int):
     n_columns = img.shape[1]
     flat_img = img.flatten()
+    # TODO make this robust to negative values
+    flat_img = np.ma.masked_array(np.nan_to_num(flat_img))
     sorted_inds = np.argsort(flat_img)
     top_n_inds = sorted_inds[-n_samples]
     i_values = top_n_inds // n_columns
@@ -49,7 +52,7 @@ class GreedyEntropyPlanner(BaseGriddedPlanner):
                 plt.scatter(next_loc[:, 1], next_loc[:, 0], c="k")
                 plt.imshow(uncertainty)
                 plt.colorbar()
-                plt.show()
+                plt.pause(0.1)
             self.predictor.update_model(next_loc, np.zeros(next_loc.shape[0]))
             plan.append(next_loc)
         plan = np.concatenate(plan, axis=0)
@@ -98,33 +101,40 @@ class GreedyEntropyPlanner(BaseGriddedPlanner):
         path = self.current_loc
         valid_locs = self.data.get_valid_loc_points()
         for i in range(n_samples):
+            # Get the upper and lower bounds for adding a new sample to the plan
             lower_bound_cost, upper_bound_cost = self._get_bounds_additional_cost(
                 path=path, candidate_locs=valid_locs
             )
+            # For now, just choose to use one
             cost = upper_bound_cost
 
+            # TODO recompute this in a better way
+            # This is how much we can add to the total cost
             additional_budget = remaining_budget * self.budget_fraction_per_sample
+            # This is how much we have for the path, including the new sample
             total_budget = pathlength - remaining_budget * (
                 1 - self.budget_fraction_per_sample
             )
 
+            # Find which ones are within budget given the cost metric
             valid_locs_within_budget = valid_locs[cost < additional_budget]
-            if vis:
-                img = self.data.get_image_for_flat_values(cost)
-                plt.plot(path[:, 1], path[:, 0])
-                plt.imshow(img)
-                plt.colorbar()
-                plt.show()
+
+            # Generate the entropy map
+            uncertainty = self.predictor.predict_values_and_uncertainty()[
+                UNCERTAINTY_KEY
+            ]
+            # Compute images
+            img = self.data.get_image_for_flat_values(cost)
+            invalid_mask = img > additional_budget
+            img[invalid_mask] = np.nan
+            uncertainty[invalid_mask] = np.nan
 
             # Sample a new loc
             valid_new_loc = False
             while not valid_new_loc:
-                candidate_new_loc = valid_locs_within_budget[
-                    np.random.choice(valid_locs_within_budget.shape[0])
-                ]
-                candidate_path = np.concatenate(
-                    (path, np.expand_dims(candidate_new_loc, axis=0)), axis=0
-                )
+                candidate_new_loc = image_argmax(uncertainty, n_samples=1)
+                print(f"Trying {candidate_new_loc}")
+                candidate_path = np.concatenate((path, candidate_new_loc), axis=0)
                 if candidate_path.shape[0] > 2:
                     ordered_candidate_path, cost = order_locations_tsp(
                         candidate_path, return_cost=True
@@ -139,13 +149,34 @@ class GreedyEntropyPlanner(BaseGriddedPlanner):
                         )
                         * 2
                     )
+                print(f"cost: {cost}, total_budget: {total_budget}")
                 if cost < total_budget:
                     path = ordered_candidate_path
                     remaining_budget = pathlength - cost
                     valid_new_loc = True
+            if vis:
+                f, axs = plt.subplots(1, 3)
+
+                axs[0].plot(path[:, 1], path[:, 0])
+                axs[1].plot(path[:, 1], path[:, 0])
+                axs[2].plot(path[:, 1], path[:, 0])
+
+                axs[0].scatter(candidate_new_loc[:, 1], candidate_new_loc[:, 0], c="k")
+                axs[1].scatter(candidate_new_loc[:, 1], candidate_new_loc[:, 0], c="k")
+                axs[2].scatter(candidate_new_loc[:, 1], candidate_new_loc[:, 0], c="k")
+
+                axs[2].imshow(self.data.image)
+
+                add_colorbar(axs[0].imshow(img))
+                add_colorbar(axs[1].imshow(uncertainty))
+                plt.show()
+            # Update the uncertainty with the new loc
+            self.predictor.update_model(
+                candidate_new_loc, np.zeros(candidate_new_loc.shape[0])
+            )
         return path.astype(int)
 
-    def plan(self, n_samples: int, pathlength=None, vis=False):
+    def plan(self, n_samples: int, pathlength=None, vis=False, vis_dist=False):
         """
         Generate samples by taking the highest entropy sample
         after fitting the model on all previous samples
@@ -159,8 +190,12 @@ class GreedyEntropyPlanner(BaseGriddedPlanner):
             _type_: plan
         """
         if pathlength is None:
-            return self._plan_unbounded(n_samples=n_samples, vis=vis)
+            path = self._plan_unbounded(n_samples=n_samples, vis=vis_dist)
         else:
-            return self._plan_bounded(
+            path = self._plan_bounded(
                 n_samples=n_samples, pathlength=pathlength, vis=vis,
             )
+        self.current_loc = path[-1:]
+        if vis:
+            self.vis(path)
+        return path
